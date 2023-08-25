a83ab8bb8807ef4c8522d560670307a5fe02b6c9
lt468 <lucatremblayuk@gmail.com>
2023-08-25 16:36:14 -0700

    completed spec

--- network/static/network/network.js
+++ network/static/network/network.js
@@ -295,8 +295,8 @@ function displayPost(post, username) {
         <span>
         ${editButton}
         <button type="button" id="${post['id']}" class="me-3 btn btn-outline-danger like-btn">
-        <svg xmlns="http://www.w3.org/2000/svg" id="heart_${post['id']}" width="16" height="16" fill="currentColor" class="bi bi-heart ${userLikedPost ? 'liked-heart' : 'unliked-heart'}" viewBox="0 0 16 16">
-        <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>
+        <svg xmlns="http://www.w3.org/2000/svg" id="heart_${post['id']}" width="16" height="16" fill="currentColor" class="bi like-btn bi-heart ${userLikedPost ? 'liked-heart' : 'unliked-heart'}" viewBox="0 0 16 16">
+        <path class="like-btn" fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>
         </svg>
         </button><span id="like-count-${post['id']}">${post['likes']}</span>
         </span>
